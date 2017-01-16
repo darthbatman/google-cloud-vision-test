da3f24f7011e36832b514794f240ea05df35d935
darthbatman <darthanakin44@gmail.com>
2017-01-16 18:30:17 -0500

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-'use strict'
-const vision = require('node-cloud-vision-api')
+const vision = require('node-cloud-vision-api');
 
-// init with auth
-vision.init({auth: 'AIzaSyAP_8CCmksJXC2H8hUnxG8xApllsMRtr08'})
+vision.init({auth: require('./config.js').key });
 
-// construct parameters
 const req = new vision.Request({
   image: new vision.Image('hackrudeviceprivacy.jpg'),
   features: [
     new vision.Feature('FACE_DETECTION', 4),
     new vision.Feature('LABEL_DETECTION', 10),
   ]
-})
+});
 
-// send single request
 vision.annotate(req).then((res) => {
-  // handling response
   console.log(JSON.stringify(res.responses))
 }, (e) => {
   console.log('Error: ', e)
-})
+});
